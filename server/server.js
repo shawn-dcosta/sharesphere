86ea2ed70fdb13c020a971ddc716d7ec5aba4f6a
shawn-dcosta <dcostashawn@gmail.com>
2025-08-12 23:46:31 +0530

    Deployment issue on render resolved

--- server/server.js
+++ server/server.js
@@ -161,5 +161,5 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 10000;
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// module.exports = app;
