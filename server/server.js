998b0ccf3268408932784166d180ce50d7322d2a
shawn-dcosta <dcostashawn@gmail.com>
2025-08-12 23:37:02 +0530

    Deployement on Render

--- server/server.js
+++ server/server.js
@@ -160,6 +160,6 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 // server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 module.exports = app;
